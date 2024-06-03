00ba4e1d3ab8f6794382149f07ace6d6c1fd0cff
PBU4685 <PBU4685@RESTITS3CL02.hca.corpad.net>
2024-06-03 12:13:15 -0400

    Initial Project Setup

---
+++ src/Quiz.java
@@ -0,0 +1,41 @@
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+
+public class Quiz implements ActionListener {
+	
+	String [] questions = {
+			
+	"Which company created Java?" , "Which year was Java created?" , "What was Java orginally called?"
+							};
+	
+	String [][] options = {
+			{"Sun Microsystems" , "Starbucks" , "Microsoft" , "Alphabet"},
+			{"1989" , "1996" , "1972" , "1492"},
+			{"Apple" , "John" , "Cliff" , "Kotlin"}
+						  };
+	
+	public Quiz() {
+		
+	}
+	
+	public void nextQuestion() {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void displayAnswer() {
+		
+	}
+	
+	public void results() {
+		
+	}
+
+}
